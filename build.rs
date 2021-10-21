ff0655375c930e8de660e0028aa681086f4f71af
Freezi <freeziftw@gmail.com>
2021-10-22 00:26:14 +0200

    Update windows bindings

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    windows::build! { }
+    windows::build! {
+        Windows::Win32::Foundation::{HINSTANCE, BOOL},
+        Windows::Win32::System::SystemServices::{
+            DLL_PROCESS_ATTACH,
+            DLL_THREAD_ATTACH,
+            DLL_THREAD_DETACH,
+            DLL_PROCESS_DETACH,
+        },
+    }
 }
