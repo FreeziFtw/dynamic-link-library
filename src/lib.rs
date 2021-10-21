b4ea01d69631ed8fd5e15ac9d6fdeb3ed5e3bb54
Freezi <freeziftw@gmail.com>
2021-10-22 00:27:47 +0200

    Add default dynamic-link-library entry

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
 windows::include_bindings!();
+
+use Windows::Win32::Foundation::*;
+use Windows::Win32::System::SystemServices::*;
+
+use std::ffi::c_void;
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "stdcall" fn DllMain(_module: HINSTANCE, reason: u32, _reserved: *mut c_void) -> BOOL {
+    match reason {
+        DLL_PROCESS_ATTACH => { }
+        DLL_THREAD_ATTACH => { }
+        DLL_THREAD_DETACH => { }
+        DLL_PROCESS_DETACH => { }
+        _ => { }
+    }
+
+    BOOL::from(true)
+}
